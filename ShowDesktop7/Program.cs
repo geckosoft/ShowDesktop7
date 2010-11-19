76f50d37399fe7b9f0bfda5a6d9981d3b14ef57e
geckosoft <tv2knet@gmail.com>
2010-11-19 17:17:11 +0100

    Added: ShowDesktop7 sources

---
+++ ShowDesktop7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShowDesktop7
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] entries)
+        {
+            Type typeShell=null;
+
+        	typeShell = Type.GetTypeFromProgID("Shell.Application");
+
+            var objShell = Activator.CreateInstance(typeShell);
+
+            typeShell.InvokeMember("MinimizeAll", System.Reflection.BindingFlags.InvokeMethod, null, objShell, null);
+        }
+    }
+}
